9edded2a42b66680924bebd90d5b20be2a4ad122
MarvinL.com <contact@marvinl.com>
2025-06-29 02:12:05 -0400

    wip: "working" prod

--- start/env.ts
+++ start/env.ts
@@ -30,7 +30,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_HOST: Env.schema.string(),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
@@ -62,5 +62,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   */
   UMAMI_SCRIPT_URL: Env.schema.string.optional(),
   UMAMI_WEBSITE_ID: Env.schema.string.optional(),
-
 })
