27e362af3d305a943b0995c38b7678fbd95c9291
MarvinL.com <contact@marvinl.com>
2025-07-11 14:43:53 -0400

    chore: update footer tagline

--- inertia/components/Footer.tsx
+++ inertia/components/Footer.tsx
@@ -31,11 +31,10 @@ export function Footer() {
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <p className="w-full text-center mb-8 lg:mb-12 text-md lg:text-lg">
-          Développé par{' '}
+          Développé entre deux écoutes d'album par{' '}
           <Link href="https://marvinl.com" target="_blank" className="text-brand">
             MarvinL.com
-          </Link>{' '}
-          entre deux écoutes d'album
+          </Link>
         </p>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand */}
